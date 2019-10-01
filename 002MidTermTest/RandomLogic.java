e2885a830476f6b68115a5ded9a040ea8bb52f3c
Lucy Gao <real.lucy.gao@gmail.com>
2019-10-01 13:40:00 -0700

    updating questions randomlogic

--- 002MidTermTest/RandomLogic.java
+++ 002MidTermTest/RandomLogic.java
@@ -7,27 +7,106 @@
  */
 public class RandomLogic
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class RandomLogic
-     */
-    public RandomLogic()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
     /**
      * An example of a method - replace this comment with your own
      *
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
+    public RandomLogic()
     {
-        // put your code here
-        return x + y;
+    }
+    
+    public void askQuestion() 
+    {
+        Counting count = new Counting();
+        
+        count.updateCounters(PlusGetsMinusGets(46, '+', 7));
+        count.updateCounters(PlusGetsMinusGets(46, '-', 7));
+        count.updateCounters(AndOr(true, "||", false));
+        count.updateCounters(AndOr(true, "&&", false));
+        tally.printCounters();
+    }
+    
+     public Boolean PlusGetsMinusGets(int arg1, char operator, int arg2)
+    {
+        // prepare question from Question class
+        Question ques = new Question();
+        
+        // format question
+        ques.question = String.format("Variable 'count' holds " + arg1 +". What is count " + "%s" + "= " + arg2 + "?", operator );
+        
+        int temp1;
+        int temp2;
+        
+        temp1 = arg1 + arg2;
+        temp2 = arg1 - arg2;
+        
+        // format question choices
+        ques.choiceA = String.format("%d",temp1);
+        ques.choiceB = String.format("%d",arg1/arg2);
+        ques.choiceC = String.format("%d",arg1*arg2);
+        ques.choiceD = String.format("%d",temp2);
+        ques.choiceE = "None of the above";
+        
+        // real time calc answer based off args AND operator
+        int ans;
+        switch(operator)
+        {
+            case '+':
+                ans = arg1 + arg2;
+                ques.answerKey = ques.answerA;
+                break;
+            case '-':
+                ans = arg1 - arg2;
+                ques.answerKey = ques.answerD;
+                break;
+            default: // not supported
+                return false;
+        }
+            
+        // format question answer based off of operation calculation
+        ques.answer =  String.format("Count holds " + arg1 + ". " + arg1 + " " + "%s " + arg2 + " = " + ans, operator);
+        
+        // getAnswer return true if question is correct
+        return ques.getAnswer();
+    }
+    
+    public Boolean AndOr(boolean arg1, String operator, boolean arg2)
+    {
+        // prepare question from Question class
+        Question ques = new Question();
+        
+        // format question
+        ques.question = String.format("What is " + arg1 + " " + "%s " + arg2 + "?", operator );
+        
+        // format question choices
+        ques.choiceA = String.format("%b",(arg1 && arg2));
+        ques.choiceB = String.format("%s", "truefalse");
+        ques.choiceC = String.format("%b",(arg1 || arg2));
+        ques.choiceD = String.format("%s","falsetrue");
+        ques.choiceE = "None of the above";
+        
+        // real time calc answer based off args AND operator
+        boolean ans;
+        switch(operator)
+        {
+            case "&&":
+                ans = (arg1 && arg2);
+                ques.answerKey = ques.answerA;
+                break;
+            case "||":
+                ans = (arg1 || arg2);
+                ques.answerKey = ques.answerD;
+                break;
+            default: // not supported
+                return false;
+        }
+            
+        // format question answer based off of operation calculation
+        ques.answer =  String.format(arg1 + " %s " + arg2 + " = " + ans, operator);
+        
+        // getAnswer return true if question is correct
+        return ques.getAnswer();
     }
 }
